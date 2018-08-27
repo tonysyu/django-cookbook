33df86d7a482c838a49fec918458c1a0e5c3b297
Tony S Yu <tsyu80@gmail.com>
2018-08-26 22:56:25 -0500

    Replace alert with notify function matching Django message

--- cookbook/static/js/project.js
+++ cookbook/static/js/project.js
@@ -1,1 +1,25 @@
 /* Project specific Javascript goes here. */
+function notify(message, cssClass) {
+    cssClass = cssClass || 'alert-info';
+
+    var $notification = $('#js-notification')
+    $notification.text(message)
+    $notification.addClass(cssClass)
+    $notification.show();
+}
+
+notify.success = function(message) {
+    notify(message, 'alert-success');
+}
+
+notify.info = function(message) {
+    notify(message, 'alert-info');
+}
+
+notify.warn = function(message) {
+    notify(message, 'alert-warning');
+}
+
+notify.error = function(message) {
+    notify(message, 'alert-danger');
+}
