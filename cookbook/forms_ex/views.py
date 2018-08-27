d7e0ca65b4ad05c772a3ef482a89c3f289c00074
Tony S Yu <tsyu80@gmail.com>
2018-08-26 20:11:43 -0500

    Add url decorator to register view routes and index links

--- cookbook/forms_ex/views.py
+++ cookbook/forms_ex/views.py
@@ -4,10 +4,32 @@ import mistune
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.urls import path
 from django.utils.html import mark_safe
 from django.views.generic import FormView, TemplateView, View
 
 from . import forms
+
+
+app_name = 'forms_ex'
+urlpatterns = []
+
+_INDEXED_VIEWS = []
+
+
+def url(url_path, name=None, title=None):
+    """Register view under the given url path."""
+    if name is None:
+        name = url_path.replace('/', '_')
+
+    def decorator(view_class):
+        urlpatterns.append(path(url_path, view_class.as_view(), name=name))
+        if title is not None:
+            view_name = f'{app_name}:{name}'
+            _INDEXED_VIEWS.append((view_name, title))
+        return view_class
+
+    return decorator
 
 
 class ConfirmAndRedirectToSelfMixin(SuccessMessageMixin):
@@ -21,6 +43,7 @@ class ConfirmAndRedirectToSelfMixin(SuccessMessageMixin):
         return redirect(self.request.path_info)
 
 
+@url('', name='index')
 class IndexView(TemplateView):
 
     template_name = 'forms_ex/index.html'
@@ -28,56 +51,86 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        view_names_and_titles = OrderedDict([
-            ('forms_ex:email_form', 'Basic form (`{{ form.as_p }}`)'),
-            ('forms_ex:email_form_as_ul', 'Basic form (`{{ form.as_ul }}`)'),
-            ('forms_ex:email_form_as_table', 'Basic form (`{{ form.as_table }}`)'),
-            ('forms_ex:email_form_crispy_filter', 'Basic form (`{{ form|crispy }}`)'),
-            ('forms_ex:email_form_crispy_tag', 'Basic form (`{% crispy form %}`)'),
-            ('forms_ex:email_modal', 'Modal form'),
-            ('forms_ex:email_modal_with_ajax_submit', 'Modal form with ajax submission'),
-            ('forms_ex:email_modal_with_ajax_helper', 'Modal form with ajax crispy helper'),
-            ('forms_ex:email_form_with_boolean', 'Basic form with boolean'),
-            ('forms_ex:crispy_form_with_onclick', 'Crispy form with onclick handler'),
-        ])
+        view_names_and_titles = _INDEXED_VIEWS
         # Update titles with markdown parser.
-        for view_name, title in view_names_and_titles.items():
-            view_names_and_titles[view_name] = mark_safe(mistune.markdown(title))
+        view_names_and_titles = [(view_name, mark_safe(mistune.markdown(title)))
+                                 for view_name, title in view_names_and_titles]
         context['view_names_and_titles'] = view_names_and_titles
 
         return context
 
 
+@url('email_form', title='Basic form (`{{ form.as_p }}`)')
 class EmailFormView(ConfirmAndRedirectToSelfMixin, FormView):
 
     template_name = 'forms_ex/email_form.html'
     form_class = forms.EmailForm
 
 
+@url('email_form_as_ul', title='Basic form (`{{ form.as_ul }}`)')
+class EmailFormAsULView(ConfirmAndRedirectToSelfMixin, FormView):
+
+    template_name = 'forms_ex/email_form_as_ul.html'
+    form_class = forms.EmailForm
+
+
+@url('email_form_as_table', title='Basic form (`{{ form.as_table }}`)')
+class EmailFormAsTableView(ConfirmAndRedirectToSelfMixin, FormView):
+
+    template_name = 'forms_ex/email_form_as_table.html'
+    form_class = forms.EmailForm
+
+
+@url('email_form_crispy_filter', title='Basic form (`{{ form|crispy }}`)')
+class EmailFormCrispyFilterView(ConfirmAndRedirectToSelfMixin, FormView):
+
+    template_name = 'forms_ex/email_form_crispy_filter.html'
+    form_class = forms.EmailForm
+
+
+@url('email_form_crispy_tag', title='Basic form (`{% crispy form %}`)')
 class EmailFormCrispyTagView(ConfirmAndRedirectToSelfMixin, FormView):
 
     template_name = 'forms_ex/email_form_crispy_tag.html'
     form_class = forms.CrispyEmailForm
 
 
+@url('email_modal', title='Modal form')
+class EmailModalView(ConfirmAndRedirectToSelfMixin, FormView):
+
+    template_name = 'forms_ex/email_modal.html'
+    form_class = forms.EmailForm
+
+
+@url('email_modal_with_ajax_submit', title='Modal form with ajax submission')
+class EmailModalWithAjaxSubmitView(ConfirmAndRedirectToSelfMixin, FormView):
+
+    template_name = 'forms_ex/email_modal_with_ajax_submit.html'
+    form_class = forms.EmailForm
+
+
+@url('email_modal_with_ajax_helper', title='Modal form with ajax crispy helper')
 class EmailFormAjaxHelperView(ConfirmAndRedirectToSelfMixin, FormView):
 
     template_name = 'forms_ex/email_modal_with_ajax_helper.html'
     form_class = forms.CrispyAjaxEmailForm
 
 
+@url('email_form_with_boolean', title='Basic form with boolean')
 class EmailFormWithBooleanView(ConfirmAndRedirectToSelfMixin, FormView):
 
     template_name = 'forms_ex/email_form_crispy_filter.html'
     form_class = forms.EmailFormWithBoolean
 
 
+@url('crispy_form_with_onclick', title='Crispy form with onclick handler')
 class CrispyFormWithOnClickView(FormView):
 
     template_name = 'forms_ex/email_form_crispy_tag.html'
     form_class = forms.CrispyFormWithOnClick
 
 
+@url('ajax_submit')
 class AjaxSubmitView(View):
 
     def post(self, request, *args, **kwargs):
